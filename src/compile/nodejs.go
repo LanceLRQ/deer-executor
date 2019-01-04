c3b5dac539229918dc8bab88dfa24a82d6ba5fd9
LanceLRQ <lancelrq@gmail.com>
2019-01-04 20:42:27 +0800

    FIX CodeCompileProvider's member.

--- src/compile/nodejs.go
+++ src/compile/nodejs.go
@@ -11,8 +11,8 @@ type NodeJSCompileProvider struct {
 
 
 func (prov *NodeJSCompileProvider) Init(code string, workDir string) error {
-	prov.isReady = false
-	prov.realTime = true
+	prov.IsReady = false
+	prov.RealTime = true
 	prov.codeContent = code
 	prov.workDir = workDir
 
@@ -28,7 +28,7 @@ func (prov *NodeJSCompileProvider) Init(code string, workDir string) error {
 func (prov *NodeJSCompileProvider) Compile() (result bool, errmsg string) {
 	result, errmsg = prov.shell(fmt.Sprintf(COMPILE_COMMAND_NODEJS, prov.codeFilePath))
 	if result {
-		prov.isReady = true
+		prov.IsReady = true
 	}
 	return
 }
