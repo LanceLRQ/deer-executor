e3a24596a7ac96f5049a32c25494542eeb7e1184
LanceLRQ <lancelrq@gmail.com>
2020-10-08 17:41:48 +0800

    进行一些基础代码的准备

---
+++ executor/runtime.go
@@ -0,0 +1,16 @@
+package executor
+
+// 基于JudgeOptions进行评测调度
+//func Run(options JudgeOptions, result *JudgeResult) error {
+//
+//}
+
+// 目标程序子进程
+func RunTargetProgramProcess() {
+
+}
+
+// 特判程序子进程
+func RunSpecialJudgeProgramProcess() {
+
+}
