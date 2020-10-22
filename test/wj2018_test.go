d1e605c5d1ba12653135b8194ee9b560ecf829d3
lanrongqi <lancelrq@gmail.com>
2020-10-22 14:43:56 +0800

    add 普通特判题单元测试样例

---
+++ test/wj2018_test.go
@@ -0,0 +1,71 @@
+package test
+
+import (
+	"github.com/LanceLRQ/deer-executor/executor"
+	"testing"
+)
+
+
+// Test: AC
+func TestWJ2018AC(t *testing.T) {
+	err := initWorkRoot()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	result, err := runWJ2018("./data/codes/WJ2018/answer_ac.c", "")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = analysisResult("wj2018-1", result, executor.JudgeFlagAC)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log("OK")
+}
+
+
+// Test: WA 1
+func TestWJ2018WA1(t *testing.T) {
+	err := initWorkRoot()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	result, err := runWJ2018("./data/codes/WJ2018/answer_wa.c", "")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = analysisResult("wj2018-2", result, executor.JudgeFlagWA)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log("OK")
+}
+
+
+
+// Test: WA 2
+func TestWJ2018WA2(t *testing.T) {
+	err := initWorkRoot()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	result, err := runWJ2018("./data/codes/WJ2018/answer_wa2.c", "")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = analysisResult("wj2018-3", result, executor.JudgeFlagWA)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log("OK")
+}
+
