f4919c609fe9bf51bf85aacd233d3f6c57b28c5e
lanrongqi <lancelrq@gmail.com>
2020-10-30 10:16:15 +0800

    add APlusB problem #2 for testlib

---
+++ data/problems/APlusB2/generators/echo.cpp
@@ -0,0 +1,9 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    cout << atoi(argv[1]) << " " << atoi(argv[2]) << endl;
+}
