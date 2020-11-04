3a0b745c8de69e1491b550996ebd25fc49e4d983
lanrongqi <lancelrq@gmail.com>
2020-11-04 17:57:26 +0800

    update checker codes

--- packmgr/checker.go
+++ packmgr/checker.go
@@ -6,22 +6,38 @@ import (
     "github.com/LanceLRQ/deer-common/utils"
     "github.com/LanceLRQ/deer-executor/executor"
     "github.com/urfave/cli/v2"
+    "io/ioutil"
     "log"
     "os"
+    "path"
 )
 
-func runSpecialJudge(session *executor.JudgeSession, caseIndex int) error {
+func runCheckerCase(session *executor.JudgeSession, caseIndex int) error {
+    iCase := &session.JudgeConfig.SpecialJudge.CheckerCases[caseIndex]
+
+
+    // 写入数据
+    err := ioutil.WriteFile(path.Join(session.SessionDir, string(caseIndex) + "_problem.in"), []byte(iCase.Input), 0666)
+    if err != nil {
+        return err
+    }
+    err = ioutil.WriteFile(path.Join(session.SessionDir, string(caseIndex) + "_problem.out"), []byte(iCase.Output), 0666)
+    if err != nil {
+        return err
+    }
+    err = ioutil.WriteFile(path.Join(session.SessionDir, string(caseIndex) + "_problem.answer"), []byte(iCase.Answer), 0666)
+    if err != nil {
+        return err
+    }
+
+    // TODO 运行checker程序
 
     return nil
 }
 
 
 func isCheckerExists(config *structs.JudgeConfiguration) error {
-    cType := "checker"
-    if config.SpecialJudge.Mode == 2 {
-        cType = "interactor"
-    }
-    cPath, err := utils.GetCompiledBinaryFileAbsPath(cType, config.SpecialJudge.Name, config.ConfigDir)
+    cPath, err := utils.GetCompiledBinaryFileAbsPath("checker", config.SpecialJudge.Name, config.ConfigDir)
     if err != nil { return err }
     _, err = os.Stat(cPath)
     if os.IsNotExist(err) {
@@ -36,12 +52,12 @@ func runSpecialJudgeChecker (session *executor.JudgeSession, caseIndex int) erro
     if caseIndex < 0 {
         for key, _ := range session.JudgeConfig.SpecialJudge.CheckerCases {
             log.Printf("[generator] run case #%d", key)
-            err := runSpecialJudge(session, key)
+            err := runCheckerCase(session, key)
             if err != nil { return err }
         }
     } else {
         log.Printf("[generator] run case #%d", caseIndex)
-        err := runSpecialJudge(session, caseIndex)
+        err := runCheckerCase(session, caseIndex)
         if err != nil { return err }
     }
     return nil
@@ -57,8 +73,8 @@ func RunSpecialJudgeChecker(c *cli.Context) error {
     session, err := executor.NewSession(configFile)
     if err != nil { return err }
 
-    if session.JudgeConfig.SpecialJudge.Mode != 1 && session.JudgeConfig.SpecialJudge.Mode != 2 {
-        return fmt.Errorf("[checker] problem not support special judge")
+    if session.JudgeConfig.SpecialJudge.Mode != 1 {
+        return fmt.Errorf("[checker] only support checker mode")
     }
 
     // checker exists?
