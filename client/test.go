14f6b521920f1c43f125c164bd9b6ea82661cbe1
lanrongqi <lancelrq@gmail.com>
2021-02-08 10:56:59 +0800

    add zip a package problem

--- client/test.go
+++ client/test.go
@@ -1,11 +1,10 @@
 package client
 
 import (
-    deer_common "github.com/LanceLRQ/deer-common"
     "github.com/urfave/cli/v2"
 )
 
 func Test(c *cli.Context) error {
-    deer_common.Test()
+
     return nil
 }
